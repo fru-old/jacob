127555c33d85d3b3e823dc83e11747285226b0ae
Florian Rüberg <florian.rueberg@gmail.com>
2017-08-18 19:48:12 +0200

    Update tree-transform.js

--- src/app/tree-transform.js
+++ src/app/tree-transform.js
@@ -3,85 +3,78 @@
 // - getChildList(item)
 // - getMultiList(item)
 class ReverseIterator {
-  constructor(tree, options) {
-    this.tree = true;
-    this.options = options;
+
+  isSelected(node, options) {
+    return options.isSelected(options.getIndex(node));
   }
 
-  isSelected(node) {
-    return this.options.isSelected(this.options.getIndex(node));
-  }
-
-  private iterateReverse(rows, parentPath) {
+  private iterateReverse(rows, parentPath, options) {
     if (!rows) return;
     for(let i = rows.length - 1; i >= 0; i--) {
       let row = rows[i];
       let rowPath = Path.getChild(parentPath, i);
 
-      let nodes = this.options.getMultiList(row);
-      let children = this.options.getChildList(row);
-      let selected = nodes.every(n => this.isSelected(n));
+      let nodes    = options.getMultiList(row);
+      let children = options.getChildList(row);
+      let selected = nodes.every(n => this.isSelected(n, options));
 
       if (!selected) {
-        this.iterateReverse(children, rowPath);
+        this.iterateReverse(children, rowPath, options);
       }
 
-      this.enterRow(row, rowPath, selected);
+      if (this.enterRow) this.enterRow(row, rowPath, selected);
       for(let j = nodes.length - 1; j >= 0; j--) {
         let node = nodes[j];
         let path = Path.getChild(parentPath, i, j);
 
-        this.visitNode(node, path, this.isSelected(node));
+        this.visitNode(node, path, this.isSelected(node, options), options);
       }
-      this.exitRow(row, rowPath, selected);
+      if (this.exitRow) this.exitRow(row, rowPath, selected);
     }
   }
 }
 
 class BuildTargetAreas extends ReverseIterator {
-  constructor(tree, options) { super(tree, options); }
 
   targets = [];
-  start() {
-    this.iterateReverse(this.tree, null);
+  start(tree, options) {
+    this.iterateReverse(tree, null, options);
     return this.targets;
   }
 
-  notSet = [];
+  notSetBefore = [];
   enterRow(row, path, selected) {
-    if (!selected) {
-      this.notSet.forEach(c => c.beforePath = path);
-      this.notSet = [];
-    }
+    if (selected) return;
+    this.notSetBefore.forEach(c => c.beforePath = path);
+    this.notSetBefore = [];
   }
 
+  previous = null;
   exitRow(row, path, selected) {
-    if (!selected) {
-      this.justAdded.forEach(c => c.afterPath = path);
-    }
+    if (!selected) this.previous = path;
   }
 
-  visitNode(node, path, selected) {
+  visitNode(node, path, selected, options) {
     let targets = []; // Generated with buildTargets from options.getBoundBox(node);
-    this.beforeNotSet = this.beforeNotSet.concat(targets);
+
+    this.notSetBefore = this.notSetBefore.concat(targets);
+    targets.forEach(c => c.afterPath = this.previous);
     this.targets = this.targets.concat(targets);
   }
 }
 
 
+class TreeTransformer {
+  insertPath, insertIndex, insertInline: true, movePath, moveIndex
+}
 
-var afterPath = null;
-var beforeNotSet = [];
+class TransformTree extends ReverseIterator {
 
-iterateRowsReverse(function (node, path, options, selected) {
-
-  if(afterPath != rowPath && !selected) {
-    beforeNotSet.forEach(c => c.beforePath = rowPath);
-    beforeNotSet = [];
+  start(tree, options, transformation) {
+    this.iterateReverse(tree, null, options);
   }
-  var targets = []; // Generated with buildTargets from options.getBoundBox(node);
 
-  targets.forEach(c => c.afterPath = rowPath);
-  beforeNotSet = beforeNotSet.concat(targets);
-  afterPath = row;
-});
+  visitNode(node, path, selected, options) {
+
+  }
+}
